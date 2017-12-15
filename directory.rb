22a58e5c1b22ce4707605ac8a546a69e1f99e7a0
philb56 <phil.makers@outlook.com>
2017-12-15 12:48:14 +0000

    week 4 step 14.2

--- directory.rb
+++ directory.rb
@@ -77,10 +77,9 @@ def load_students(filename = "students.csv")
 end
 ###################################
 def try_load_students
-  filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
-    load_students(filename)
+  filename = ARGV.first.nil? ? "students.csv" : ARGV.first
+  if File.exists?(filename)
+     load_students(filename)
      puts "Loaded #{@students.count} from #{filename}"
   else # if it doesn't exist
     puts "Sorry, #{filename} doesn't exist."
